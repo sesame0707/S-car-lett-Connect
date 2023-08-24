4907c9ea963b79c93e3ae7ce35875b7936b4fda7
sesame0707 <petergrala@gmail.com>
2023-08-24 22:30:39 +0200

    Minor UI improvements done

--- AndroidStudioProjects/ScarlettConnect/app/build.gradle.kts
+++ AndroidStudioProjects/ScarlettConnect/app/build.gradle.kts
@@ -34,10 +34,14 @@ android {
 
 dependencies {
 
+    val navVersion = "2.5.3"
+
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.8.0")
+    implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
+    implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
