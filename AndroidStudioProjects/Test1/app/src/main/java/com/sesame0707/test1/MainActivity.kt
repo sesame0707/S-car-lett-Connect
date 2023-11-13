e75f23184b5fbeb1d094a46eb173dce8e4fe648a
sesame0707 <petergrala@gmail.com>
2023-11-13 23:47:23 +0100

    Test 1 app added
    Simple app to learn Wi-Fi Direct socket communication

---
+++ AndroidStudioProjects/Test1/app/src/main/java/com/sesame0707/test1/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.sesame0707.test1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import java.net.Socket
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            Toast.makeText(this@MainActivity, "You clicked me.", Toast.LENGTH_SHORT).show()
+//            val client = Socket("192.168.4.3", 12345)
+//            client.getOutputStream().write(byteArrayOf(77, 100))
+        }
+    }
+}
