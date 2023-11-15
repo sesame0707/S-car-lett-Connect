4ff2f43e7ff8617d87da2a333211fdc1264be503
sesame0707 <petergrala@gmail.com>
2023-11-15 20:47:02 +0100

    Wi-Fi Direct in Test 1 app fixed

--- AndroidStudioProjects/Test1/app/src/main/java/com/sesame0707/test1/MainActivity.kt
+++ AndroidStudioProjects/Test1/app/src/main/java/com/sesame0707/test1/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.sesame0707.test1
 
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
+import android.os.StrictMode
+import android.os.StrictMode.ThreadPolicy
 import android.widget.Button
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import java.net.Socket
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,9 +16,16 @@ class MainActivity : AppCompatActivity() {
 
         val button = findViewById<Button>(R.id.button)
         button.setOnClickListener {
-            Toast.makeText(this@MainActivity, "You clicked me.", Toast.LENGTH_SHORT).show()
-//            val client = Socket("192.168.4.3", 80)
-//            client.getOutputStream().write(byteArrayOf(77, 100))
+            val SDK_INT = Build.VERSION.SDK_INT
+            if (SDK_INT > 8) {
+                val policy = ThreadPolicy.Builder()
+                    .permitAll().build()
+                StrictMode.setThreadPolicy(policy)
+            }
+//            Toast.makeText(this@MainActivity, "You clicked me.", Toast.LENGTH_SHORT).show()
+            val client = Socket("192.168.4.3", 80)
+            client.getOutputStream().write(byteArrayOf(77, 100))
+            client.close()
         }
     }
 }
