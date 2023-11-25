df9ec4bf7ad8caf839d93fcae5c902ddf92d2a8a
sesame0707 <petergrala@gmail.com>
2023-11-25 23:06:49 +0100

    Preference objects in Settings view added
    It caused issues:
    1) "[Issue] App crashes when rotating screen at Settings view and About view"
    2) "[Issue] PreferenceCategory in Settings view doesn't work"

--- AndroidStudioProjects/ScarlettConnect/app/build.gradle.kts
+++ AndroidStudioProjects/ScarlettConnect/app/build.gradle.kts
@@ -42,6 +42,7 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.preference:preference-ktx:1.2.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
