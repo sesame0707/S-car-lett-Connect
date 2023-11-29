5602252d5a9887a8f2585f8794ff7ee399e0029c
sesame0707 <petergrala@gmail.com>
2023-11-29 17:15:56 +0100

    "Make onClickListener() for Preference objects" half-done
    TODO: Add new Intent to open Wi-Fi settings

--- AndroidStudioProjects/ScarlettConnect/app/src/main/java/com/sesame0707/scarlettconnect/ui/settings/SettingsFragment.kt
+++ AndroidStudioProjects/ScarlettConnect/app/src/main/java/com/sesame0707/scarlettconnect/ui/settings/SettingsFragment.kt
@@ -1,11 +1,28 @@
 package com.sesame0707.scarlettconnect.ui.settings
 
 import android.os.Bundle
+import android.util.Log
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import com.sesame0707.scarlettconnect.R
 
 class SettingsFragment : PreferenceFragmentCompat() {
+    private var prefSettingsConnect: Preference? = null
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
+
+        prefSettingsConnect = findPreference("pref_settings_connect")
+
+        prefSettingsConnect?.setOnPreferenceClickListener {
+            Log.d("Preferences", "Feedback was clicked")
+            true // Return true if the click is handled.
+        }
+//        prefSettingsConnect?.setOnPreferenceChangeListener { preference, newValue ->   true
+//
+//        Log.d(
+//            "TAG",
+//            "(BOM) onCreate(SettingsFragment:PreferenceManager.findPreference):SettingsFragment"
+//        )
+//        true
     }
 }
